810c5c92ca59cb81fc1d791299830f9ea5f000ee
Husky <husky77d@163.com>
2023-08-24 18:16:43 +0800

    '注入map与properties'

--- Spring-003/src/main/java/com/Husky/spring/Bean/Person.java
+++ Spring-003/src/main/java/com/Husky/spring/Bean/Person.java
@@ -1,12 +1,15 @@
 package com.Husky.spring.Bean;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 public class Person {
     private List<String> names;
     private Set<String> addrs;
-
+    private Map<Integer,String> phones;
+    private Properties sqlDriver;
     public void setNames(List<String> names) {
         this.names = names;
     }
@@ -15,11 +18,21 @@ public class Person {
         this.addrs = addrs;
     }
 
+    public void setPhones(Map<Integer, String> phones) {
+        this.phones = phones;
+    }
+
+    public void setSqlDriver(Properties sqlDriver) {
+        this.sqlDriver = sqlDriver;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
                 "names=" + names +
                 ", addrs=" + addrs +
+                ", phones=" + phones +
+                ", sqlDriver=" + sqlDriver +
                 '}';
     }
 }
