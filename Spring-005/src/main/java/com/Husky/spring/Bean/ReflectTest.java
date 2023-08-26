e91944f8542baa3ea70bacbf07576c1d5bba74a2
Husky <husky77d@163.com>
2023-08-26 23:09:37 +0800

    '反射机制赋值修改'

--- Spring-005/src/main/java/com/Husky/spring/Bean/ReflectTest.java
+++ Spring-005/src/main/java/com/Husky/spring/Bean/ReflectTest.java
@@ -5,11 +5,16 @@ import java.lang.reflect.Method;
 
 public class ReflectTest {
     public static void main(String[] args) throws Exception{
-        Class<?> clazz = Class.forName("com.Husky.spring.Bean.User");
-        Method setName = clazz.getDeclaredMethod("setName", String.class);
-        Method setAge  = clazz.getDeclaredMethod("setAge", int.class);
+        String property1="name";
+        String property2="age";
+        String Method1="set"+property1.toUpperCase().charAt(0)+property1.toLowerCase().substring(1);
+        String Method2="set"+property2.toUpperCase().charAt(0)+property2.toLowerCase().substring(1);
+        String className="com.Husky.spring.Bean.User";
+        Class<?> clazz = Class.forName(className);
         Field age = clazz.getDeclaredField("age");
         Field name=clazz.getDeclaredField("name");
+        Method setName = clazz.getDeclaredMethod(Method1, name.getType());
+        Method setAge  = clazz.getDeclaredMethod(Method2, age.getType());
         Object o = clazz.getConstructor().newInstance();
         setName.invoke(o,"Husky");
         setAge.invoke(o,22);
--- Spring-005/src/main/java/com/Husky/spring/Bean/User.java
+++ Spring-005/src/main/java/com/Husky/spring/Bean/User.java
@@ -11,12 +11,8 @@ public class User {
     public void setAge(int age) {
         this.age = age;
     }
-
     @Override
     public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+        return name+"今年"+age+"岁";
     }
 }
